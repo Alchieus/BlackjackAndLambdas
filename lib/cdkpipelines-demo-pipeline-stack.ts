4af44b1f1d403e9238644270bdbdc8e42c64bb6e
Smith, James (CAI - Mission) <James.Smith2@coxautoinc.com>
2021-01-27 21:35:07 -0600

    Add Manual Approval

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,5 +1,5 @@
-import * as codepipeline from '@aws-cdk/aws-codepipeline';
-import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { Artifact, IAction} from '@aws-cdk/aws-codepipeline';
+import { GitHubSourceAction, ManualApprovalAction} from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
 import { CdkpipelinesDemoStage } from "./cdkpipelines-demo-stage"
@@ -12,10 +12,10 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     super(scope, id, props);
 
     // sourceArtifact holds the source code from the repo
-    const sourceArtifact = new codepipeline.Artifact();
+    const sourceArtifact = new Artifact();
 
     // cloudAssemblyArtifact holds the result of the build pipeline, the built code, the cloudformation template, etc
-    const cloudAssemblyArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new Artifact();
  
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       // The pipeline name
@@ -23,7 +23,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       cloudAssemblyArtifact,
 
       // Where the source can be found
-      sourceAction: new codepipeline_actions.GitHubSourceAction({
+      sourceAction: new GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
         oauthToken: SecretValue.secretsManager('AlchieusBlackJackAndLambdasAwsLabAccessToken'),
@@ -32,22 +32,29 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         branch: "main"
       }),
 
-       // How it will be built and synthesized
-       synthAction: SimpleSynthAction.standardNpmSynth({
-         sourceArtifact,
-         cloudAssemblyArtifact,        
-         // We need a build step to compile the TypeScript Lambda
-         buildCommand: 'npm run build'
-       }),
+      // How it will be built and synthesized
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,        
+        // We need a build step to compile the TypeScript Lambda
+        buildCommand: 'npm run build'
+      }), 
     });
+
 
     // This is where we add the application stages
     // ...
-    pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, "Pre-Prod", {
-      env:{
-        account: "881385135648",
-        region: "us-east-1"
-      }
+    const preprodStage = pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, "Pre-Prod", {
+      env:props?.env,
     }));
+
+    const actions: IAction[] = [];
+    actions.push(new ManualApprovalAction({
+      actionName: "RequestLabDeployApproval",
+      notifyEmails: ["James.Smith2@Coxautoinc.com"],
+      runOrder: 1
+    }));
+
+    preprodStage.addActions(...actions);
   }
 }
